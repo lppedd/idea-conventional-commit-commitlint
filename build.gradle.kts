dbe96ec60c0ceef481cda4c10ff07498aebeca8b
Edoardo Luppi <lp.edoardo@gmail.com>
2022-06-07 10:32:02 +0200

    build: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,29 @@
+@file:Suppress("ConvertLambdaToReference")
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.jetbrains.intellij") version "0.4.26"
-  kotlin("jvm") version "1.4.20-M1"
+  id("org.jetbrains.intellij") version "1.6.0"
+  kotlin("jvm") version "1.7.0-RC2"
 }
 
 group = "com.github.lppedd"
 version = "0.1.2"
 
 repositories {
-  maven("https://dl.bintray.com/kotlin/kotlin-eap")
   mavenCentral()
 }
 
 intellij {
-  version = "IU-2019.2"
-  downloadSources = true
-  pluginName = "idea-conventional-commit-commitlint"
-  setPlugins("com.github.lppedd.idea-conventional-commit:0.15.3")
+  type.set("IU")
+  version.set("2019.2")
+  downloadSources.set(true)
+  pluginName.set("idea-conventional-commit-commitlint")
+  plugins.set(listOf("com.github.lppedd.idea-conventional-commit:0.15.3"))
 }
 
 dependencies {
-  compileOnly(kotlin("stdlib-jdk8", "1.4.20-M1"))
+  compileOnly(kotlin("stdlib-jdk8", "1.7.0-RC2"))
 }
 
 tasks {
@@ -38,10 +40,12 @@ tasks {
   compileTestKotlin(kotlinOptions)
 
   patchPluginXml {
-    version(project.version)
-    sinceBuild("192")
-    untilBuild(null)
-    pluginDescription(File("plugin-description.html").readText(Charsets.UTF_8))
-    changeNotes(File("change-notes/${version.replace('.', '_')}.html").readText(Charsets.UTF_8))
+    version.set(project.version.toString())
+    sinceBuild.set("192")
+    untilBuild.set(null as String?)
+
+    val projectPath = projectDir.path
+    pluginDescription.set((File("$projectPath/plugin-description.html").readText(Charsets.UTF_8)))
+    changeNotes.set((File("$projectPath/change-notes/${version.get().replace('.', '_')}.html").readText(Charsets.UTF_8)))
   }
 }
